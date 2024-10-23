c4cd39113da93e1dee33ee44b84654ef8c801ae0
dansaa8 <dansaa8@protonmail.com>
2024-10-23 17:39:51 +0200

    Add project files.

---
+++ Storage/Data/StorageContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Storage.Models;
+
+namespace Storage.Data
+{
+    // En klass som använder sig av EntityFramework(ORM mappar mellan databasen och vår C# kod).
+    public class StorageContext : DbContext
+    {
+        public StorageContext (DbContextOptions<StorageContext> options)
+            : base(options)
+        {
+        }
+
+        // Denna property använder vi oss av för att hämta data från databasen
+        public DbSet<Storage.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ Storage/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Storage.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+// Våran StorageContext registreras som en service. Läggs till i våran dependency injector container
+// så att vi har åtkomst till den i resten av applikationen.
+builder.Services.AddDbContext<StorageContext>(options =>
+    // Här bestäms att SQL-server ska användas som databas.
+    // Som argument skickas connection strängen som vi hämtar från våran appsettings.json.
+    // Slutligen skickas konfigurationen som ett argument till konstruktorn i StorageContext.
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StorageContext") ?? throw new InvalidOperationException("Connection string 'StorageContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
