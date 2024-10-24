10b35183611151d021e0cf39594e786ee3d26b69
dansaa8 <dansaa8@protonmail.com>
2024-10-24 20:02:38 +0200

    Add data annotations to Product class. Add a ViewModel for Product. Add Link to Products in header.

--- Storage/Models/Product.cs
+++ Storage/Models/Product.cs
@@ -1,14 +1,34 @@
-﻿namespace Storage.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "A product name is required")]
+        [DisplayName("Product Name")]
+        [StringLength(20)]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "A price must be set")]
+        [Range(0.01, 2000.00,
+            ErrorMessage = "Price must be between 0.01 and 2000.00")]
         public double Price { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime OrderDate { get; set; }
+
+        [Required(ErrorMessage = "A category must be provided")]
         public string Category { get; set; }
+
+        [Required(ErrorMessage = "A shelf must be entered")]
         public string Shelf { get; set; }
+
+        [Required(ErrorMessage = "An item count must be provided")]
         public int Count { get; set; }
+
         public string Description { get; set; }
     }
 }
---
+++ Storage/Models/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Storage.Models
+{
+    public class ProductViewModel
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Count { get; set; }
+        public int InventoryValue { get; set; }
+    }
+}
